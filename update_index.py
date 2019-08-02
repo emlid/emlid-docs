e5dd615cc68b2804410401ccc7079f0c151466e5
Gleb Fedorov <glebfedorov888@gmail.com>
2019-08-02 22:42:40 +0000

    update_index.py: add template building script
    Script updates index.md file for every provided config

--- update_index.py
+++ update_index.py
@@ -9,14 +9,15 @@ env = Environment(
         loader=FileSystemLoader("templates/"),
         autoescape=select_autoescape(["html", "md"])
 )
+
 for filename in os.listdir("conf"):
     data = {}
     with open("conf/" + filename, "r") as conf:
         try:
-            if filename != "config_rtk.yml":
-                data = yaml.safe_load(conf)
-                template = env.get_template(sys.argv[1].split('/')[1])
-                print(template.render(data))
+            data = yaml.safe_load(conf)
+            template = env.get_template("index_template.md")
+            with open("docs/reach/" + filename.split("_")[1].split(".")[0] + "/index.md", "w") as dest:
+                dest.write(template.render(data))
         except yaml.YAMLError as exc:
                 if hasattr(exc, 'problem_mark'):
                      mark = exc.problem_mark
